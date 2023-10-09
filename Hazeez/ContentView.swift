b1474e55dee0d45d542f3874fbe56db7e582464f
leena <leena1759@hotmail.com>
2023-10-09 15:19:55 +0300

    Initial Commit

---
+++ Hazeez/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hazeez
+//
+//  Created by leena alyami on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hazeez/HazeezApp.swift
@@ -0,0 +1,17 @@
+//
+//  HazeezApp.swift
+//  Hazeez
+//
+//  Created by leena alyami on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct HazeezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
